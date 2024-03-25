a731382776b608c210e303ac900f242669bcee90
jorge <jorgemartinezpizarro@gmail.com>
2024-03-25 19:24:01 +0100

    fix error serializing BitIng

--- src/app/api/pitagoreanTree/route.ts
+++ src/app/api/pitagoreanTree/route.ts
@@ -4,6 +4,7 @@ export async function GET(request: Request) {
   
   const { searchParams } = new URL(request.url)
   const LIMIT = BigInt(searchParams.get('LIMIT') || "0")
-
+  // https://github.com/GoogleChromeLabs/jsbi/issues/30#issuecomment-953187833
+  BigInt.prototype.toJSON = function() { return this.toString() }
   return Response.json( pitagoreanTree(BigInt(LIMIT)) )
 }
