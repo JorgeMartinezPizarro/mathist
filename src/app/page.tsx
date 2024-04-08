be5d1cc358e0fc0ada92ed682c6363d3105b2695
jorge <jorgemartinezpizarro@gmail.com>
2024-04-08 09:40:36 +0200

    add route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,7 @@
 'use client'
 
-import { useEffect, useState } from "react"
-import { Tab, Box } from '@mui/material';
-import { TabPanel, TabList, TabContext }  from '@mui/lab';
+import { redirect } from 'next/navigation'
 
-import PrimeFactorization from "./components/PrimeFactorization";
-import PitagoreanTree from "./components/PitagoreanTree";
-import SerieDifferences from "./components/SerieDifferences";
-import EratostenesSieve from "./components/EratostenesSieve";
-
-export default function Home() {
-  
-  const [value, setValue] = useState("4")
-
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue)
-  }
-
-  return <>
-    <TabContext value={value}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <TabList onChange={handleChange} aria-label="lab API tabs example" centered>
-          <Tab label="Sieve" value="4" />
-          <Tab label="Tree" value="2" />
-          <Tab label="Factors" value="1" />
-          <Tab label="Series" value="3" />
-        </TabList>
-      </Box>
-      <TabPanel value="4"><EratostenesSieve /></TabPanel>
-      <TabPanel value="1"><PrimeFactorization /></TabPanel>
-      <TabPanel value="2"><PitagoreanTree /></TabPanel>
-      <TabPanel value="3"><SerieDifferences /></TabPanel>
-    </TabContext>
-  </>
+export default () => {
+    redirect("/sieve", "push");
 }
