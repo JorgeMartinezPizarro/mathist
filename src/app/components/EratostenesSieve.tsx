214e82c5b6361fd6976c8e2f002b9727a417ba17
jorge <jorgemartinezpizarro@gmail.com>
2024-04-11 14:06:52 +0200

    improve even more visualization on small screens

--- src/app/components/EratostenesSieve.tsx
+++ src/app/components/EratostenesSieve.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Button, CircularProgress, TextField, Alert, FormGroup  } from "@mui/material"
+import { Button, CircularProgress, TextField, Alert, FormGroup } from "@mui/material"
 import { useState } from "react"
 
 import {default as d} from "@/helpers/duration"
