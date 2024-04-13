0d431f91c0d88cc50f8de748258ee4d3c494a669
jorge <jorgemartinezpizarro@gmail.com>
2024-04-13 02:29:37 +0200

    fix healthy limits for randomPrimes. Add comment about a wrong prime generated.

--- src/app/api/randomPrimes/route.ts
+++ src/app/api/randomPrimes/route.ts
@@ -1,7 +1,5 @@
-import { MAX_ALLOCATABLE_MATRIX_30GB, MAX_DISPLAY_SIEVE, MAX_LENGTH_FOR_SIEVE_HEALTY } from '@/helpers/Constants'
-import eratostenes from '@/helpers/eratostenes'
+import { MAX_DIGITS_RANDOM_PRIMES } from '@/helpers/Constants'
 import randomPrimes from '@/helpers/randomPrimes'
-import toHuman from '@/helpers/toHuman'
 
 export async function GET(request: Request) {
   
@@ -15,15 +13,10 @@ export async function GET(request: Request) {
       throw new Error("Invalid parameters length = " + LIMIT + ", amount = " + amount)
     }
 
-    if (amount > 10) {
-      throw new Error("Too much numbers, max amount allowed is 10")
+    if (amount * LIMIT > MAX_DIGITS_RANDOM_PRIMES) {
+      throw new Error("Invalid parameters length = " + LIMIT + ", amount = " + amount + " the max total of DIGITS is " + MAX_DIGITS_RANDOM_PRIMES)
     }
 
-    if (LIMIT > 600) {
-      throw new Error("Too long numbers, max length is 600 digits")
-    }
-
-    if (amount)
     (BigInt.prototype as any).toJSON = function() {
       return this.toString()
     }
--- src/helpers/randomPrimes.tsx
+++ src/helpers/randomPrimes.tsx
@@ -3,28 +3,49 @@ import { bignumber, isPrime } from "mathjs";
 import { isMillerRabinProbablePrime, isBaillieProbablePrime } from "./primalyTests"
 import id from '@/helpers/id';
 import getTimeMicro from "./getTimeMicro";
+import duration from "./duration";
 
 // The random generator
 // Inspired in https://bigprimes.org/how-it-works
 // Be aware above 200 digits it can takes a lot of time
 const randomPrimes = (length: number, amount: number) => {
+
     const start = getTimeMicro();
+    
+    let elapsed = getTimeMicro();
+    let countFailedAttemps = 0
     // Generate really big primes, above 300 digits it takes long!
     const primes: bigint[] = []
     while (primes.length < amount) {
-        const number: bigint = BigInt(id(length))
-        // Check the baillie test
-        if (isBaillieProbablePrime(number)) {
+        const string: string = id(length)
+        const number: bigint = BigInt(string)
+        // Check the baillie test only if the number did not start with 0
+        if (string[0] !== "0" && isBaillieProbablePrime(number)) {
             primes.push(number)
+        }
+        else {
+            countFailedAttemps++;
         }
     }
 
+    console.log("Failed attemps " + countFailedAttemps + ", now checking primality by the hard way")
+    elapsed = getTimeMicro();
+    
     primes.forEach(prime => {
-        // Check again with baillie and even with a deterministic function
-        if (!isMillerRabinProbablePrime(prime) && !isPrime(bignumber(prime.toString()))) {
-            throw new Error("Invalid prime generated, what a bad luck!. Prime: " +  prime)
+        
+        console.log("Testing if is prime " + prime.toString())
+        /*if (!isMillerRabinProbablePrime(prime)) {
+            throw new Error("Invalid prime generated, what a bad luck!. Composite: " +  prime)
+        }*/
+        /*
+        // A deterministic check is funny up to 50 digits ... furthermore it take years
+        if (!isPrime(bignumber(prime.toString()))) {
+            throw new Error("Invalid prime generated, what a bad luck!. Composite: " +  prime)
         }
+        */
     })
+
+    console.log("Validating primality for " + duration(getTimeMicro() - elapsed))
     
     return {
         primes,
