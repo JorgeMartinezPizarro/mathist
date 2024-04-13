9a68d3f64e889837be0fb685961e98b03a9d2187
jorge <jorgemartinezpizarro@gmail.com>
2024-04-14 01:37:18 +0200

    fix layout issue

--- src/helpers/NumberToString.tsx
+++ src/helpers/NumberToString.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Button } from "@mui/material";
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 import locale from "./locale";
@@ -12,11 +12,11 @@ function NumberToString(props: NumberToStringProps) {
     const {number} = props;
     
     return (<>
-        <Typography title={"It is a " + number.toString().length + " digits number. Also " +  locale(BigInt(number))}>
+        <span title={"It is a " + number.toString().length + " digits number. Also " +  locale(BigInt(number))}>
             {number.toString()}
             &nbsp;
             {number.toString().length > 20 && <Button title="Click here to copy the number"><ContentCopyIcon onClick={() => {navigator.clipboard.writeText(number.toString())}} /></Button>}
-        </Typography>
+        </span>
         
     
     </>);
