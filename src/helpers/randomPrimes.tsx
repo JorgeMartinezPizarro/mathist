01b6160fa19c75fbf0944f1fe539a1a5e698b861
jorge <jorgemartinezpizarro@gmail.com>
2024-04-13 02:34:23 +0200

    add second primality test to ensure results

--- src/helpers/randomPrimes.tsx
+++ src/helpers/randomPrimes.tsx
@@ -4,6 +4,7 @@ import { isMillerRabinProbablePrime, isBaillieProbablePrime } from "./primalyTes
 import id from '@/helpers/id';
 import getTimeMicro from "./getTimeMicro";
 import duration from "./duration";
+import { FALSE_BIG_PRIME } from "./Constants";
 
 // The random generator
 // Inspired in https://bigprimes.org/how-it-works
@@ -11,6 +12,8 @@ import duration from "./duration";
 const randomPrimes = (length: number, amount: number) => {
 
     const start = getTimeMicro();
+
+    console.log(isPrime(bignumber(FALSE_BIG_PRIME.toString())))
     
     let elapsed = getTimeMicro();
     let countFailedAttemps = 0
@@ -33,16 +36,14 @@ const randomPrimes = (length: number, amount: number) => {
     
     primes.forEach(prime => {
         
-        console.log("Testing if is prime " + prime.toString())
-        /*if (!isMillerRabinProbablePrime(prime)) {
-            throw new Error("Invalid prime generated, what a bad luck!. Composite: " +  prime)
-        }*/
-        /*
-        // A deterministic check is funny up to 50 digits ... furthermore it take years
-        if (!isPrime(bignumber(prime.toString()))) {
+        // Test with miller-rabin
+        if (!isMillerRabinProbablePrime(prime)) {
             throw new Error("Invalid prime generated, what a bad luck!. Composite: " +  prime)
         }
-        */
+        // A deterministic check would be cool but after 30 digits it takes years
+        /*if (!isPrime(bignumber(prime.toString()))) {
+            throw new Error("Invalid prime generated, what a bad luck!. Composite: " +  prime)
+        }*/
     })
 
     console.log("Validating primality for " + duration(getTimeMicro() - elapsed))
