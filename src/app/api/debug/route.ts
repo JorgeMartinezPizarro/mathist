b68bb2bfd66c86e055fa1cbc5e9934e0f1e23b64
Jorge Martinez Pizarro <jorgemartinerzpizarro@gmail.com>
2024-05-24 16:26:40 +0200

    move debug to mersenne endpoint. Add additional debug endpoint to test different multiplication algorithms. Add c++ to LLT benchmark reports

--- src/app/api/debug/route.ts
+++ src/app/api/debug/route.ts
@@ -1,97 +1,80 @@
 // TODO: review:
-// https://en.wikipedia.org/wiki/Lucas%E2%80%93Lehmer_primality_test#Proof_of_correctness
-// https://github.com/preda/gpuowl
+// Understand demonstration: https://en.wikipedia.org/wiki/Lucas%E2%80%93Lehmer_primality_test#Proof_of_correctness
+// Investigate GPU usage: https://github.com/preda/gpuowl
+// http://www.polprimos.com/
 import os from 'node:os' 
 import fs from 'fs' 
-import fetch from 'node-fetch';
-import { Worker, isMainThread } from 'worker_threads';
 import _ from "lodash"
-
-import errorMessage from '@/helpers/errorMessage'
-import getTimeMicro from '@/helpers/getTimeMicro'
+import getTimeMicro from '@/helpers/getTimeMicro';
+import errorMessage from '@/helpers/errorMessage';
 import duration from '@/helpers/duration';
-import eratosthenes from '@/helpers/eratosthenes';
-import { KNOWN_MERSENNE_PRIMES, MERSENNE_TABLE } from '@/Constants';
+import id from "@/helpers/id"
+import { karatsuba } from '@/helpers/multiplication';
 import series from '@/helpers/series';
 import differences from '@/helpers/differences';
-import { lucasLehmerTest } from '@/helpers/isMersennePrime';
-
-export interface MersennePrime {
-  p: number;
-  isPrime: boolean;
-}
-
-export interface MersenneReport {
-  language: string;
-  maxPrime: number;
-  time: number;
-  mersennePrimes: MersennePrime[];
-}
-
-//https://en.wikipedia.org/wiki/Pocklington_primality_test
-
-//https://es.wikipedia.org/wiki/N%C3%BAmero_primo_de_Mersenne
 
 export async function GET(request: Request): Promise<Response> {
 
-  const start = getTimeMicro();
-  
-  (BigInt.prototype as any).toJSON = function() {
-    return this.toString()
-  }
-
   try {
+    const start = getTimeMicro()
+    let elapsed = getTimeMicro();
+    let filename = `benchmark.html`
     
-    // TODO: use a temp file to record the processed values. Make the search resumable. Maybe we can just pack all this search into Scala code and use the mather just to link to the results.
-    ////////////////////////////////////////////////////////////////////////////
-    // Benchmark Scala VS Go vs Js bigint
-    ////////////////////////////////////////////////////////////////////////////
     const { searchParams } = new URL(request.url||"".toString())
     const KEY: string = searchParams.get('KEY') || "";
-    const LIMIT: number = parseInt(searchParams.get('maxPrime') || "128");
-    const numberOfThreads: number = parseInt(searchParams.get('numberOfThreads') || "16");
-    const language: string = searchParams.get("lang") || "go"
-    const mode = searchParams.get("mode") || "mersenne";
+    const mode = searchParams.get("mode") || ""
+    const LIMIT = parseInt(searchParams.get("LIMIT") || "0")
     
+    let strings = []
+
     if (KEY !== process.env.MATHER_SECRET?.trim()) {
       throw new Error("Forbidden!");
     }
 
-    
-    let strings: string[] = []
-
-    let filename = `/files/debug_${mode}_${numberOfThreads}_${language}_${LIMIT}.html`
-
-    if (mode === "mersenne") {
-      const languages = language === "all" ? ["rust", "go", "scala"/*, "javascript"*/] : [language]
-      const numbers = eratosthenes(LIMIT, LIMIT).primes.map(p=>Number(p))
-      strings = await mersennePrimesBenchmark(numbers, numberOfThreads, languages)
-    } else if (mode === "mersenne-check") {
-    const languages = language === "all" ? ["rust", "go", "scala", /*"javascript"*/] : [language]
-      const numbers = KNOWN_MERSENNE_PRIMES.slice(0, LIMIT)
-      strings = await mersennePrimesBenchmark(numbers, numberOfThreads, languages)
-    }
-    else if (mode === "primes") {
+    if (mode === "primes") {
       strings = await primesDifferences(LIMIT)
-      filename = "/files/primes.html"
-    } else if (mode === "json") {
-      filename = "/files/data.json"
-      const numbers = eratosthenes(LIMIT, LIMIT).primes.map(n => Number(n))
-      const objects = numbers.map((p: number, i: number) => ({n: i+1, pn: p}))
-      fs.writeFileSync("./public/files/data.json", JSON.stringify(objects))
-      return Response.json({message: "Report generated under " + filename, time: getTimeMicro() - start})
+      filename = "primes.html"
     } else {
-      throw new Error("Unknown mode " + mode);
+      const t = 3
+
+
+      for (var i = 0; i < t; i++) {
+        console.log(i + " / " + (t * 2))
+        const a = BigInt(id(25 * 10**6))
+        const b = BigInt(id(25 * 10**6))
+        a * b
+      }
+
+      const bigIntMultiplicationTime = getTimeMicro() - elapsed
+
+      elapsed = getTimeMicro()
+
+      for (var i = 0; i < t; i++){
+        console.log(i + t + " / " + (t*2))
+        const a = BigInt(id(25 * 10**6))
+        const b = BigInt(id(25 * 10**6))
+        karatsuba(a, b)
+      }
+
+      const karatsubaTime = getTimeMicro() - elapsed
+
+      strings = [
+        "<p style='text-align: center;'>Multiply 2 numbers with 25 million digits with bigint multiplication takes in average " + duration(Math.round((bigIntMultiplicationTime) / t)) + "</p>",
+        "</hr>",
+        "<p style='text-align: center;'>Multiply 2 numbers with 25 million digits with karatsuba multiplication takes in average " + duration(Math.round((karatsubaTime) / t)) + "</p>",
+        "</hr>",
+      ];
+      filename= "benchmark.html"
     }
+    
+    const filepath = "./public/files/" + filename
 
-    const filepath = "./public" + filename
-
+    
     const stringArray = [
-      "<h3 style='text-align: center;'>Debug report of mather.ideniox.com</h3>",
+      "<h3 style='text-align: center;'>Benchmark report of mather.ideniox.com</h3>",
       "<p style='text-align: center;'><b>" + os.cpus()[0].model + " " + (os.cpus()[0].speed/1000) + "GHz , " + os.cpus().length + " cores, " + process.arch + "</b></p>",
       "<hr/>",
-      ...strings,
-      "</hr>",
+      ...strings,  
       "<p style='text-align: center;'>It took " + duration(getTimeMicro() - start) + " to generate the report</p>",
       "<hr/>",
     ]
@@ -117,7 +100,7 @@ async function primesDifferences(LIMIT: number): Promise<string[]> {
 
   const table = result[0].map((number, i) => "<tr><td style='text-align: center;'>" + (i + 1)  + "</td><td style='text-align: center;'>" + number + "</td><td style='text-align: center;'>" + result[1][i] + "</td><td style='text-align: center;'>" + result[2][i] + "</td></tr>")
   
-  const strings = [
+  return [
     "<table style='width: 500px; margin: 0 auto;'><thead>",
     "<tr><th>n</th><th>Pn</th><th>Δ(Pn)</th><th>Δ2(Pn)</th></tr>",
     "</thead>",
@@ -125,327 +108,5 @@ async function primesDifferences(LIMIT: number): Promise<string[]> {
     ...table,
     "</tbody></table>",
   ]
-  return strings
 }
 
-async function mersennePrimesBenchmark(numbers: number[], numberOfThreads: number, languages: string[]): Promise<string[]> {
-    
-    let elapsed = getTimeMicro();
-    const mersenneReport: MersenneReport[] = []
-
-    if (languages.includes("rust")) {
-      const mersennePrimesRust = (await (computeMersenneRust(numbers, numberOfThreads, numberOfThreads))).filter(p=>p.isPrime)
-      const timeForRustLLTP = getTimeMicro() - elapsed
-      mersenneReport.push(
-        {language: "rust", maxPrime: numbers.slice(-1)[0], time: timeForRustLLTP, mersennePrimes: mersennePrimesRust}
-      )
-      elapsed = getTimeMicro()
-    } 
-    if (languages.includes("go")) {
-      const mersennePrimesGo = (await (computeMersenneGo(numbers, numberOfThreads, numberOfThreads))).filter(p=>p.isPrime)
-      const timeForGoLLTP = getTimeMicro() - elapsed
-      mersenneReport.push(
-        {language: "go", maxPrime: numbers.slice(-1)[0], time: timeForGoLLTP, mersennePrimes: mersennePrimesGo}
-      )
-      elapsed = getTimeMicro()
-    } 
-    if (languages.includes("javascript")) {
-      const mersennePrimesJS = (await computeMersenneJS(numbers, numberOfThreads, numberOfThreads)).filter(p=>p.isPrime)
-      const timeForJSLLTP = getTimeMicro() - elapsed
-      elapsed = getTimeMicro()    
-
-      mersenneReport.push(
-        {language: "javascript", maxPrime: numbers.slice(-1)[0], time: timeForJSLLTP, mersennePrimes: mersennePrimesJS}
-      )
-      elapsed = getTimeMicro()
-    } 
-    if (languages.includes("scala")) {
-      const mersennePrimesScala = await computeMersenneScala(numbers, 500, numberOfThreads)
-      const timeForScalaLLTP = getTimeMicro() - elapsed
-      mersenneReport.push(
-        {language: "scala", maxPrime: numbers.slice(-1)[0], time: timeForScalaLLTP, mersennePrimes: mersennePrimesScala}
-      )
-      elapsed = getTimeMicro()
-    }  
-    
-    for (var i = 0; i<mersenneReport.length - 1; i++) {
-      if (!_.isEqual(mersenneReport[i].mersennePrimes, mersenneReport[i+1].mersennePrimes)) {
-        console.log("////////////////////////////////////////////////")
-        console.log(mersenneReport[i].mersennePrimes)
-        console.log("////////////////////////////////////////////////")
-        console.log(mersenneReport[i+1].mersennePrimes)
-        throw new Error("WTF underteministic computation!")
-      }
-    }
-
-    if (mersenneReport.length < 1) {
-      return []
-    }
-
-    const mersennePrimesRow = mersenneReport[0].mersennePrimes.map(mp => {
-      const mersenneRow = MERSENNE_TABLE.find(mr => mr.prime === mp.p)
-      return "<tr><td style='text-align: center'>" + mersenneRow?.position + "</td><td style='text-align: center'>2**" + mersenneRow?.prime + "-1</td><td style='text-align: center'>" + mersenneRow?.discoveryDate + "</td><td style='text-align: center'>" + mersenneRow?.discoveredBy + "</td></tr>"
-    })
-
-    const benchmarkRows = mersenneReport.reduce((acc: string[], val: MersenneReport) => {
-      return [...acc,
-        "<hr/>",
-        "<p style='text-align: center;'><b>"  + val.language + "</b></p>",
-        "<hr/>",
-        "<p style='text-align: center;'>It took " + duration(val.time) + " to test " + numbers.length + " primes using " + numberOfThreads + " threads.",
-
-      ]
-    }, [])
-
-    const stringArray = [
-      "<table style='margin: 0 auto; width: 700px;'><thead><tr><th>#</th><th>Mersenne prime</th><th>Date</th><th>Finder</th></tr></thead><tbody>",
-      ...mersennePrimesRow,
-      "</tbody></table>",
-      "<hr/>",
-      "<p style='text-align: center;'>" + mersenneReport[0].mersennePrimes.length + " Mersenne primes found</p>",
-      ...benchmarkRows,
-      "<hr/>",
-    ]
-
-    return stringArray
-}
-
-///////////////////////////////////////////////////////////
-// Use scala for the computation!
-///////////////////////////////////////////////////////////
-async function computeMersenneScala(primesArray: number[], batchSize: number, numThreads: number): Promise<MersennePrime[]> {
-  
-  const mersennePrimes: MersennePrime[] = new Array();
-  
-  for (let i = 0; i < primesArray.length; i += batchSize) {
-      const batch = primesArray.slice(i, i + batchSize);
-      const response = await computeLLTPScala(batch, numThreads);
-      mersennePrimes.push(...response)
-  }
-  
-  return mersennePrimes.sort((mp1, mp2) => mp1.p - mp2.p)
-}
-
-async function computeLLTPScala(primes: number[], numThreads: number): Promise<MersennePrime[]>  {
-  
-  const url = 'http://37.27.102.105:8080/lltp';
-
-  const options = {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({
-        numbers: primes.join(","),
-        numThreads,
-    }),
-    timeout: 86400 * 1000, // A day. No timeouts wanted.
-  }
-
-  console.log("Requesting", url, options)
-
-  const response = await fetch(url, options)
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status} ${response.toString()}`);
-  }
-
-
-  const x: any = (await response.json())
-
-  return x.filter((p: MersennePrime) => p.isPrime)
-}
-
-///////////////////////////////////////////////////////////
-// Use Js for the computation
-///////////////////////////////////////////////////////////
-async function computeMersenneJS(primesArray: number[], batchSize: number, numberOfThreads: number): Promise<MersennePrime[]> {
-  const mersennePrimes: MersennePrime[] = [];
-  // single thread
-  /*return primesArray.map(p => ({
-    isPrime: lucasLehmerTest(p),
-    p,
-  }))*/
-  // Process primes in batches
-  for (let i = 0; i < primesArray.length; i += batchSize) {
-      const batch = primesArray.slice(i, i + batchSize);
-
-      try {
-          // Await the completion of the current batch computation
-          const response = await computeLLTPJs(batch, numberOfThreads);
-          // Push the results of the current batch to the mersennePrimes array
-          mersennePrimes.push(...response);
-      } catch (error) {
-          console.error(`Error processing batch. ` + errorMessage(error));
-          // You may choose to handle or log the error here
-      }
-  }
-
-  // Sort the mersennePrimes array before returning
-  return mersennePrimes.sort((mp1, mp2) => mp1.p - mp2.p);
-}
-
-async function computeLLTPJs(numbers: number[], numThreads: number): Promise<MersennePrime[]> {
-  if (isMainThread) {
-    // This is the main thread
-
-    //console.log("Requesting " + numbers.length + " numbers up to " + numbers.slice(-1)[0] + "  with numThreads = " + numThreads)
-    
-    const workerPromises: Promise<MersennePrime>[] = []; // Adjusted to hold single promises
-    let currentIndex = 0;
-
-    // Function to create a new worker
-    const createWorker = (number: number) => {
-      const worker = new Worker('./src/app/api/debug/thread.mjs', {
-        workerData: number
-      });
-
-      // Create a promise that resolves with the result from the worker
-      const workerPromise = new Promise<MersennePrime>((resolve, reject) => { // Adjusted to resolve single MersennePrime
-        // Listen for messages from the worker
-        worker.on('message', message => {
-          // Assuming the worker sends back a single MersennePrime object
-          resolve(message as MersennePrime);
-        });
-
-        // Handle errors
-        worker.on('error', error => {
-          reject(error);
-        });
-
-        // Handle worker exit
-        worker.on('exit', code => {
-          if (code !== 0) {
-            reject(new Error(`Worker stopped with exit code ${code}`));
-          }
-        });
-      });
-
-      // Push the worker promise to the array
-      workerPromises.push(workerPromise);
-    };
-
-    // Start the workers
-    for (const number of numbers) {
-      createWorker(number);
-    }
-
-    // Return a promise that resolves when all workers are done
-    return Promise.allSettled(workerPromises).then(results => {
-      const mergedResults: MersennePrime[] = [];
-      for (const result of results) {
-        if (result.status === 'fulfilled') {
-          // Push the resolved MersennePrime object to the mergedResults array
-          mergedResults.push(result.value);
-        } else {
-          console.error(`Worker promise rejected: ${result.reason}`);
-        }
-      }
-      return mergedResults.sort((a, b) => a.p - b.p);
-    });
-  } else {
-    // This is a worker thread, not the main thread
-    console.error('This script should be run as the main thread.');
-    return [];
-  }
-}
-
-///////////////////////////////////////////////////////////
-// Use GO for the computation!
-///////////////////////////////////////////////////////////
-async function computeMersenneGo(primesArray: number[], batchSize: number, numThreads: number): Promise<MersennePrime[]> {
-  
-  const mersennePrimes: MersennePrime[] = new Array();
-  
-  for (let i = 0; i < primesArray.length; i += batchSize) {
-      const batch = primesArray.slice(i, i + batchSize);
-      const response = await computeLLTPGo(batch, numThreads);
-      mersennePrimes.push(...response)
-  }
-  
-  return mersennePrimes.sort((mp1, mp2) => mp1.p - mp2.p)
-}
-
-async function computeLLTPGo(primes: number[], numThreads: number): Promise<MersennePrime[]>  {
-  
-  const url = 'http://37.27.102.105:5002/lltp';
-
-  const options = {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({
-        numbers: primes.join(","),
-        num_threads: numThreads,
-    }),
-    timeout: 86400 * 1000, // A day. No timeouts wanted.
-  }
-
-  console.log("Requesting", url, options)
-
-  const response = await fetch(url, options)
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status} ${response.toString()}`);
-  }
-
-  const x: any = (await response.json())
-
-  return x.filter((p: MersennePrime) => p.isPrime).map((mp: any) => {return {isPrime: mp.isPrime, p: Number(mp.p)}})
-}
-
-///////////////////////////////////////////////////////////
-// Use RUST for the computation!
-///////////////////////////////////////////////////////////
-async function computeMersenneRust(primesArray: number[], batchSize: number, numThreads: number): Promise<MersennePrime[]> {
-  
-  const mersennePrimes: MersennePrime[] = new Array();
-  
-  for (let i = 0; i < primesArray.length; i += batchSize) {
-      const batch = primesArray.slice(i, i + batchSize);
-      const response = await computeLLTPRust(batch, numThreads);
-      mersennePrimes.push(...response)
-  }
-  
-  return mersennePrimes.sort((mp1, mp2) => mp1.p - mp2.p)
-}
-
-async function computeLLTPRust(primes: number[], numThreads: number): Promise<MersennePrime[]>  {
-  
-  const url = 'http://37.27.102.105:3030/llt';
-
-  const options = {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({
-        numbers: primes.map(p => p.toString()),
-        number_of_threads: numThreads,
-    }),
-    timeout: 86400 * 1000, // A day. No timeouts wanted.
-  }
-
-  console.log("Requesting", url, options)
-
-  const response = await fetch(url, options)
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status} ${await response.text()}`);
-  }
-
-  const x: any = (await response.json())
-
-  console.log(x)
-
-  const y = x.filter((p: any) => p.is_prime || p.p === '2')
-
-  console.log(y)
-
-  const z = y.map((mp: any) => {return {isPrime: mp.is_prime || mp.p === '2', p: Number(mp.p)}})
-
-  console.log(z)
-
-  return z
-}
--- src/helpers/isMersennePrime.ts
+++ src/helpers/isMersennePrime.ts
@@ -1,14 +1,16 @@
 import percent from './percent';
 import duration from './duration';
 import getTimeMicro from './getTimeMicro';
-
+import { isMillerRabinProbablePrime } from './isProbablePrime';
+import { karatsuba } from "./multiplication"
+import bigInt from "big-integer"
 // TODO: add the logic here to test faster PRP before https://github.com/preda/gpuowl
 // Function to perform the Lucas-Lehmer test
 function lucasLehmerTest(p: number) {
-
+    console.log("Start lucas lehmer test for p = " + p)
     const start = getTimeMicro()
     
-    if (p === 2) return true; // 2^2 - 1 = 3 is prime
+    if (p === 2 || p === 3) return true; // 2^2 - 1 = 3 is prime, 2^3-1 = 7 is also prime
 
     if (p <= 1 || p % 2 === 0) return false; // Mersenne number for p <= 1 or even p is not prime
 
@@ -17,7 +19,7 @@ function lucasLehmerTest(p: number) {
     let s = BigInt(4);
     
     for (let i = 3; i <= p; i+=1) {
-        s = (s*s - BigInt(2)) % mersenneNumber
+        s = BigInt(magmaSquare(s.toString())) - BigInt(2) & mersenneNumber
         process.stdout.write("\r");
         process.stdout.write("\r");
         process.stdout.write("LL: Tested " + percent(BigInt(i), BigInt(p)) + " in " + duration(getTimeMicro() - start) +  " " + p +  "               ")
@@ -27,9 +29,96 @@ function lucasLehmerTest(p: number) {
     process.stdout.write("\r");
     process.stdout.write("LL: Tested 100.000% in " + duration(getTimeMicro() - start) +  " " + p +  "               \n")
 
-    return s % mersenneNumber === BigInt(0)
+    return s === BigInt(0)
 }
 
+// Function to pad a string with leading zeros
+function padZeros(str: string, length: number): string {
+    return '0'.repeat(Math.max(0, length - str.length)) + str;
+}
+
+// Function to split a string into two halves
+function splitHalf(str: string): [string, string] {
+    const half = Math.ceil(str.length / 2);
+    return [str.slice(0, half), str.slice(half)];
+}
+
+// Function to add two strings representing numbers
+function add(a: string, b: string): string {
+    let carry = 0;
+    let result = '';
+    const maxLength = Math.max(a.length, b.length);
+    a = padZeros(a, maxLength);
+    b = padZeros(b, maxLength);
+
+    for (let i = maxLength - 1; i >= 0; i--) {
+        const sum = parseInt(a[i]) + parseInt(b[i]) + carry;
+        result = (sum % 10).toString() + result;
+        carry = Math.floor(sum / 10);
+    }
+
+    if (carry > 0) {
+        result = carry.toString() + result;
+    }
+
+    return result;
+}
+
+// Function to subtract two strings representing numbers
+function subtract(a: string, b: string): string {
+    let borrow = 0;
+    let result = '';
+    const maxLength = Math.max(a.length, b.length);
+    a = padZeros(a, maxLength);
+    b = padZeros(b, maxLength);
+
+    for (let i = maxLength - 1; i >= 0; i--) {
+        let diff = parseInt(a[i]) - parseInt(b[i]) - borrow;
+        if (diff < 0) {
+            diff += 10;
+            borrow = 1;
+        } else {
+            borrow = 0;
+        }
+        result = diff.toString() + result;
+    }
+
+    return result.replace(/^0+/, '');
+}
+
+// Function to multiply two strings representing numbers
+function multiply(a: string, b: string): string {
+    if (a.length === 0 || b.length === 0) {
+        return '0';
+    }
+
+    const maxLength = Math.max(a.length, b.length);
+    a = padZeros(a, maxLength);
+    b = padZeros(b, maxLength);
+
+    if (maxLength === 1) {
+        return (parseInt(a) * parseInt(b)).toString();
+    }
+
+    const half = Math.ceil(maxLength / 2);
+    const [aHigh, aLow] = splitHalf(a);
+    const [bHigh, bLow] = splitHalf(b);
+
+    const z0 = multiply(aLow, bLow);
+    const z1 = multiply(add(aHigh, aLow), add(bHigh, bLow));
+    const z2 = multiply(aHigh, bHigh);
+
+    const step1 = z2 + '0'.repeat(maxLength);
+    const step2 = subtract(subtract(z1, z2), z0) + '0'.repeat(half);
+    const result = add(add(step1, step2), z0);
+
+    return result;
+}
+
+// Function to square a large number using the Magma squaring algorithm
+function magmaSquare(x: string): string {
+    return multiply(x, x);
+}
 
 
 export { lucasLehmerTest }
